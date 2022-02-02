6a46376adb7b0d795d943f1409eceab9708d7ae5
Porins <porinslauris@gmail.com>
2022-02-02 13:39:48 +0200

    Ieviestas metodes

--- src/Uzdevums.java
+++ src/Uzdevums.java
@@ -2,57 +2,51 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Uzdevums {
-	public static void main(String[] args) {
-		int studSk, kritSk;
-		Scanner scan = new Scanner(System.in);
-		DecimalFormat df = new DecimalFormat("0.#");
-		     
-		do {
-			System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
-			studSk = scan.nextInt();
-		}while(studSk<1);
-		String[] studenti = new String[studSk];
-		
-		do {
-			System.out.println("Kāds būs kritēriju skaits?");
-			kritSk = scan.nextInt();
-		}while(kritSk<1);
-		String[] kriteriji = new String[kritSk];
-		int[] kriterijaSvars = new int[kritSk];
-		int[][] kriterijaVertejums = new int[studSk][kritSk];
-		double[] semestraVertejums = new double[studSk];
-		
+	static Scanner scan = new Scanner(System.in);
+	public static String[] ievadit(String[] studenti){
 		//Ievada studentu vārdus, uzvārdus
 		for(int i=0; i<studenti.length; i++) {
 			System.out.println("Ievadi "+(i+1)+". studentu");
 			studenti[i] = scan.next();
 		}
-		
+		return studenti;
+	}
+	public static String[] ievaditKriteriju(String[] kriteriji ){
 		//Definē kritērijus
+				for(int i=0; i<kriteriji.length; i++) {
+					System.out.println("Ievadi "+(i+1)+". kritēriju");
+					kriteriji[i] = scan.next();
+	}
+				return kriteriji;
+	}
+	public static int[] svars(int[] kriterijaSvars, int kritSk){
+		//Norāda katra kritērija svaru
 		int maxSvars = 100;
-		for(int i=0; i<kriteriji.length; i++) {
-			System.out.println("Ievadi "+(i+1)+". kritēriju");
-			kriteriji[i] = scan.next();
-			//Norāda katra kritērija svaru
-			do {
-				System.out.println("Ievadi "+(i+1)+". kritērija svaru");
-				kriterijaSvars[i] = scan.nextInt();
-			}while(kriterijaSvars[i]>maxSvars || 
-					kriterijaSvars[i]<1 || 
-					(kriterijaSvars[0]==100 && kritSk > 1));
-			maxSvars -= kriterijaSvars[i];
-		}
-		
-		//Norāda vērtējumu kādu ieguvis katrs students par katru kritēriju
-		for(int i=0; i<kriterijaVertejums.length; i++) {
-			for(int j=0; j<kriterijaVertejums[i].length; j++) {
+		for(int i=0; i<kriterijaSvars.length; i++) {
 				do {
-					System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
-					kriterijaVertejums[i][j] = scan.nextInt();
-				}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
+					System.out.println("Ievadi "+(i+1)+". kritērija svaru");
+					kriterijaSvars[i] = scan.nextInt();
+				}while(kriterijaSvars[i]>maxSvars || 
+						kriterijaSvars[i]<1 || 
+						(kriterijaSvars[0]==100 && kritSk > 1));
+				maxSvars -= kriterijaSvars[i];
 			}
-		}
-		
+		return kriterijaSvars;
+	}
+	public static int[][] ievadaVertejumu(int[][] kriterijaVertejums, String[] studenti, String[] kriteriji){
+		//Norāda vērtējumu kādu ieguvis katrs students par katru kritēriju
+				for(int i=0; i<kriterijaVertejums.length; i++) {
+					for(int j=0; j<kriterijaVertejums[i].length; j++) {
+						do {
+							System.out.println("Ievadi "+studenti[i]+" vērtējumu par kritēriju "+kriteriji[j]);
+							kriterijaVertejums[i][j] = scan.nextInt();
+						}while(kriterijaVertejums[i][j]<0 || kriterijaVertejums[i][j]>10);
+					}
+				}
+		return kriterijaVertejums;
+	}
+	public static double[] izrekinat(int[][] kriterijaVertejums, String[] studenti, int[] kriterijaSvars, double[] semestraVertejums,String[] kriteriji){
+		DecimalFormat df = new DecimalFormat("0.#");  
 		double rezultats;
 		for(int i=0; i<studenti.length; i++) {
 			rezultats=0;
@@ -68,6 +62,36 @@ public class Uzdevums {
 			}
 			System.out.println("Semestra vērtējums ir "+df.format(semestraVertejums[i])+"\n");
 		}
+		return semestraVertejums;
+	}
+	public static void main(String[] args) {
+		int studSk, kritSk,izvele;
+		do{
+			System.out.println("1-Cik studenti un cik kritēriji | 2-Studentu ievadīšana | 3-Kritēriju ievadīšana |  5 - Apturēt programmu");
+			izvele = scan.nextInt();
+			switch(izvele){
+			case 1:
+				do {
+					System.out.println("Cik studentiem aprēķināsi gala vērtējumu?");
+					studSk = scan.nextInt();
+				}while(studSk<1);
+				String[] studenti = new String[studSk];	
+				ievadit(studenti);
+				do {
+					System.out.println("Kāds būs kritēriju skaits?");
+					kritSk = scan.nextInt();
+				}while(kritSk<1);
+				String[] kriteriji = new String[kritSk];
+				ievaditKriteriju(kriteriji);
+				int[] kriterijaSvars = new int[kritSk];
+				svars(kriterijaSvars,kritSk);
+				int[][] kriterijaVertejums = new int[studSk][kritSk];
+				ievadaVertejumu(kriterijaVertejums,studenti,kriteriji);
+				double[] semestraVertejums = new double[studSk];
+				izrekinat(kriterijaVertejums,studenti,kriterijaSvars,semestraVertejums, kriteriji);
+				break;
+			}
+		}while(izvele!=7);
 		scan.close();
 	}
 }
